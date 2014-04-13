b6a6f07cea5a50c0df51e68039a6bfc61233f66d
davewalk <daviddwalk@gmail.com>
2014-04-12 22:11:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Electronic Intern
+
+An electronic intern for [PhillyASAP](http://phillyasap.org/) using Twilio's voice API. A Work in progress, more details soon!
---
+++ tests.py
@@ -0,0 +1,28 @@
+import unittest, requests, os
+import xml.etree.ElementTree as ET
+
+class TestCase(unittest.TestCase):
+	def test_intro_message(self):
+		r = requests.get('http://localhost:8000/intro')
+		root = ET.fromstring(r.content)
+		assert root[0].tag == 'Gather'
+		assert root[0][0].tag == 'Say'
+		assert root[0].attrib['action'] == '/forward'
+		assert root[0].attrib['method'] == 'GET'
+		assert r.headers['content-type'] == 'application/xml'
+
+	def test_forward_correct_digit(self):
+		r = requests.get('http://localhost:8000/forward?Digits=4')
+		root = ET.fromstring(r.content)
+		assert root[0].tag == 'Say'
+		assert root[1].tag == 'Dial'
+		assert root[1].text == os.environ['PHILLYASAP_FORWARD_NO']
+
+	def test_forward_incorrect_digit(self):
+		r = requests.get('http://localhost:8000/forward?Digits=5')
+		root = ET.fromstring(r.content)
+		assert root[0].tag == 'Gather'
+		assert root[1].tag == 'Say'
+
+if __name__ == '__main__':
+	unittest.main()
