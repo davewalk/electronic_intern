8fb4a4c223b9774e03d2e12ac3a7192c35b63c03
Dave Walk <daviddwalk@gmail.com>
2014-07-08 21:04:43 -0400

    Adding Mongo for response logging

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
 import os
 from flask import Flask, make_response, request, redirect, url_for
 from jinja2 import Environment, FileSystemLoader
+import pymongo
+from datetime import datetime
+
+MONGO_URL = os.environ.get('MONGOHQ_URL')
+
+if MONGO_URL:
+    conn = pymongo.Connection(MONGO_URL)
+    db = conn[urlparse(MONGO_URL).path[1:]]
+else:
+    conn = pymongo.Connection('localhost', 27017)
+    db = conn['intern']
 
 app = Flask(__name__)
 env = Environment(autoescape=True,
@@ -12,6 +23,9 @@ app.debug = False
 @app.route('/intro', methods=['GET', 'POST'])
 def intro():
     """Play the intro message."""
+
+    db.responses.insert({"to": to, "what": "response", "date": datetime.now()})
+
     resp_xml = env.get_template('intro.xml')
     mp3_url = os.environ['MP3_URL']
     resp = make_response(resp_xml.render(mp3_url=mp3_url))
@@ -27,10 +41,14 @@ def forward():
         - If 4, repeat the message.
     """
     digit = request.values.get('Digits', None)
+    to = request.values.get('To', None)
+
+    db.responses.insert({"to": to, "digit": int(digit), "date": datetime.now()})
+
     if digit == '1':
         resp_xml = env.get_template('forward.xml')
         forward_no = os.environ['PHILLYASAP_FORWARD_NO']
-        resp = make_response(resp_xml.render(forward_no=forward_no))
+        resp = make_response(resp_xmlrrender(forward_no=forward_no))
         resp.headers['Content-Type'] = 'application/xml'
         return resp
     elif digit == '4':
